859ad9bbfd5178cf1ed0de0807dfb3f62645e0f8
GIVENALITY <vgiven@yahoo.com>
2025-10-08 10:34:29 +0300

    bugfix

--- app/Models/FuelRequest.php
+++ app/Models/FuelRequest.php
@@ -255,7 +255,7 @@ class FuelRequest extends Model
 
     public function canBeDispensed()
     {
-        return $this->status === self::STATUS_APPROVED;
+        return in_array($this->status, [self::STATUS_PENDING, self::STATUS_APPROVED]);
     }
 
     public function canBeCompleted()
